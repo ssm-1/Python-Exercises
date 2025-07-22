137f8668896ba0e5675766a16b35d93d38153eb8
ssm-1 <68942461+ssm-1@users.noreply.github.com>
2025-07-22 19:28:25 +0530

    Add files via upload

---
+++ removefirstnchars.py
@@ -0,0 +1,8 @@
+#Exercise 4: Remove first n characters from a string
+
+userstring = input(print("Enter the string: \n"))
+number = int(input(print("enter a number, n:")))
+
+print(userstring[number:])
+
+
